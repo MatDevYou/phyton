dc6208f2eee4a0ac3850142db00b36c777707570
MatDevYou <cod4882@gmail.com>
2024-04-16 12:01:38 +0200

    a

--- Lezioni/Lez1/Iterazione/Quinto.py
+++ Lezioni/Lez1/Iterazione/Quinto.py
@@ -5,3 +5,15 @@ Titolo:Scrivere un programma che chieda quanti alunni ci sono in una classe poi 
 fa inserire M voti, M dato in input, e ne scrive la media.
 
 '''
+
+alunni = int(input("Quanti studenti ci sono? "))
+M = int(input("Quanti voti per ogni studente? "))
+
+for i in range(alunni):
+    totale_voti = 0
+    print("\nAlunno", i+1)
+    for j in range(M):
+        voto = float(input("Inserisci il voto : "))
+        totale_voti += voto
+    media = totale_voti / M
+    print("Media dell'alunno", i+1, ":", media)
--- Lezioni/Lez1/Selezione - Testo Esercizi/Quinto.py
+++ Lezioni/Lez1/Selezione - Testo Esercizi/Quinto.py
@@ -5,3 +5,18 @@ Titolo: Un negoziante per ogni spesa di importo superiore a 100 € effettua uno
 l’importo risulta superiore a 300€ lo sconto è del 10%. Scrivere un programma che richieda
 all'utente l'ammontare della spesa e visualizzi quindi l'importo effettivo da pagare.
 '''
+#dichiarazione variabili
+spesa = float(input("Inserisci importo della spesa: "))
+
+#se la spesa è superiore di 300 applico uno sconto di 10%
+if spesa > 300:
+    sconto = spesa * 0.10
+    totale = spesa - sconto
+    print("Spesa: ",spesa,"Sconto: 10%",sconto,  "Totale: ", totale )
+elif spesa > 100:
+#se la spesa è superiore di 300 applico uno sconto di 5%
+    sconto = spesa * 0.05
+    totale = spesa - sconto
+    print("Spesa: ",spesa,"Sconto: 5%",sconto,  "Totale: ", totale )
+else:
+    print("Nessuno sconto applicabile perche la spesa è inferiore a 100")
