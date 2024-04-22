00bdf6c357eb5fd28bf20c5379631fd74828178e
MatDevYou <cod4882@gmail.com>
2024-04-22 12:56:15 +0200

    py

--- Lezioni/Lez2/s.py
+++ Lezioni/Lez2/s.py
@@ -3,5 +3,5 @@ def areaRettangolo(base: float, altezza: float ) -> float:
 
 print("Area 1 = ", areaRettangolo(3.2,4.5))
 print("Area 2 = ", areaRettangolo(5,4))
-print("Area 3 = ", areaRettangolo(“A”,3))
-print("Area 4 = ", areaRettangolo(“A”,”B”))
+print("Area 3 = ", areaRettangolo("A",3))
+print("Area 4 = ", areaRettangolo("A","B"))
