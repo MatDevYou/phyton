17223756aae614b1225e4073a3c513bbf484053f
MatDev <cod4882@gmail.com>
2024-04-30 00:14:34 +0200

    esercizi python lez 3

---
+++ Lezioni/Lez3/esercizi/lista/quarto.py
@@ -0,0 +1,19 @@
+'''
+Autore: Matteo Meringolo
+Data: 20/04/2024
+Titolo: 
+Scrivere un programma Python per dividere una data lista in due parti in cui viene data la
+lunghezza della prima parte della lista.
+Esempio:
+Lista originale: [1, 1, 2, 3, 4, 4, 5, 1]
+Lunghezza della prima parte della lista: 3
+Output : Prima parte: [1, 1, 2] ,
+Seconda parte: [3, 4, 4, 5, 1]
+'''
+
+#inizializzo lista
+lista = [1, 1, 2, 3, 4, 4, 5, 1]
+
+
+print(lista[0:3])
+print(lista[3:9])
