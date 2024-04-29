4cef963b4e3cec117e9dfb40471f895701e1b05a
MatDevYou <cod4882@gmail.com>
2024-04-29 10:56:34 +0200

    lez 3 spiegazione stringhe

---
+++ Lezioni/Lez3/stringhe/modificareCarattere.py
@@ -0,0 +1,16 @@
+str = "Stringa"
+str[0] = 'X'
+print(str)
+
+#le stringhe sono immutabili
+
+str = "Stringa"
+print(str)
+str = "Altra"
+print(str)
+str += "suffisso"
+print(str)
+str *= 3
+print(str)
+
+#pero cosi si perche non modifica la stringa ma sostituisci str
---
+++ Lezioni/Lez3/stringhe/modoStampaStringaAscii.py
@@ -0,0 +1,10 @@
+str = "Stringa"
+for carattere in str :
+    print(carattere,": ",ord(carattere))
+    
+for carattere in "CIAO" :
+    print(carattere)
+    
+
+for carattere in range(1,4) :
+    print(carattere)
