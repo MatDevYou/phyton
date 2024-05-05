695d55d7e3d9a28cb239cdeea4c4b51977f9e68c
MatDev <cod4882@gmail.com>
2024-05-05 22:35:24 +0200

    modifiche es stringa 1

--- Lezioni/Lez3/esercizi/stringa/primo.py
+++ Lezioni/Lez3/esercizi/stringa/primo.py
@@ -7,19 +7,25 @@ stringa modificata.
 '''
 
 def rimuoviCarattere(stringa, n):
+    '''
+    Funzione rimuoveeCarattere
+
+    Parametri:
+    strinza -> ritorna la stringa che andiamo a verificare
+    n -> indica la posizione della stringa che vogliamo rrimuovere
+    '''
     # Verifica se l'indice n è valido
     if n < 0 or n >= len(stringa):
         return "Errore: Indice non valido"
     
-    # Utilizza lo slicing per creare una nuova stringa senza il carattere in posizione n
     nuova_stringa = stringa[:n] + stringa[n+1:]
     
     return nuova_stringa
 
-# Esempio di utilizzo della funzione
+#dichiaro la stringa da verificare 
 str = "ciao"
 n = 2  # Rimuovere il terzo carattere ('a')
 stringaMod = rimuoviCarattere(str, n)
-print(str)  
+print(stringaMod)  
 
 
