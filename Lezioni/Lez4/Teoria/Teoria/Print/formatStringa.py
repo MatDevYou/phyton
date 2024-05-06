9985742592c0b62014c25a72726a772a0787fa2a
MatDevYou <cod4882@gmail.com>
2024-05-06 11:54:52 +0200

    a

---
+++ Lezioni/Lez4/Teoria/Teoria/Print/formatStringa.py
@@ -0,0 +1,8 @@
+str1 = 'a'
+num2 = 10
+str3 = 'abc'
+str4 = 'abcd'
+print(f'{str1:>10}')
+print(f'{num2:>10}')
+print(f'{str3:<10}')
+print(f'{str4:>10}')
---
+++ Lezioni/Lez4/Teoria/Teoria/Print/funzione.py
@@ -0,0 +1,9 @@
+def somma(a,b):
+    return a+b
+
+n1=10
+n2=20
+print(f"n1 incrementato di 2: {n1+1}")
+print(f"n1/n2 {n1/n2:.2f}")
+print(f"n1+n2={somma(n1,n2)}")
+print(f"{somma(n1,n2)=}")
