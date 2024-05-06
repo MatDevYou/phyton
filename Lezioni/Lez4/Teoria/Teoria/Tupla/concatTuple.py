f8a1861c68a7578f9a0b08d12dbbcd8e4ca6eff9
MatDevYou <cod4882@gmail.com>
2024-05-06 11:39:08 +0200

    AS

---
+++ Lezioni/Lez4/Teoria/Teoria/Tupla/concatTuple.py
@@ -0,0 +1,7 @@
+lista = [2,3]
+lista += ('c',2.3)
+print(type(lista))
+print(lista)
+tupla = (2,3)
+tupla += ['c',2.3]
+print(tupla)
---
+++ Lezioni/Lez4/Teoria/Teoria/Tupla/modTupla.py
@@ -0,0 +1,7 @@
+tupla = ()
+tupla += (3,)
+tupla += ('c',2.3)
+print(tupla)
+tupla2 = (10, 30)
+tupla3 = tupla + tupla2
+print(tupla3)
