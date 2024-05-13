d3663e20faaa1bd6c4c7c37997fc7e7e981ad013
MatDevYou <cod4882@gmail.com>
2024-05-13 12:48:51 +0200

    a

---
+++ Lezioni/Lez5/Esercizi/Quarto.py
@@ -0,0 +1,8 @@
+'''
+
+Autore: Matteo Meringolo
+Data: 13/05/2024
+Titolo:Scrivete un programma Python per rimuovere i duplicati dal dizionario.
+
+'''
+
--- Lezioni/Lez5/Esercizi/Secondo.py
+++ Lezioni/Lez5/Esercizi/Secondo.py
@@ -1,4 +1,3 @@
-import random
 '''
 
 Autore: Matteo Meringolo
