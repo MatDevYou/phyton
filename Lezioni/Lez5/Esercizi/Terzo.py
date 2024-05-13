2644ea7f9faf57eb679969880dfeb3037e496285
MatDevYou <cod4882@gmail.com>
2024-05-13 12:45:55 +0200

    a

--- Lezioni/Lez5/Esercizi/Terzo.py
+++ Lezioni/Lez5/Esercizi/Terzo.py
@@ -8,4 +8,8 @@ Titolo:Scrivete un programma Python per ottenere il valore massimo e minimo in u
 '''
 
 diz1 = {'v1':1,'v2':2,'v3':3}
-print(diz1)
+
+valoreMax = max(diz1)
+valoreMin = min(diz1)
+
+print("Valore massimo:",valoreMax, "Valore minimo:", valoreMin)
