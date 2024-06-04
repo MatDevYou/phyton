096d1b2296585276131d70fbb9a1c054eeb47d34
MatDevYou <cod4882@gmail.com>
2024-06-04 16:57:12 +0200

    a

--- Lezioni/Lez8/Teoria/altriModiPerLeggere.py
+++ Lezioni/Lez8/Teoria/altriModiPerLeggere.py
@@ -1,4 +1,17 @@
 fh = open("file.txt")
-righe = fh.read()
+righe = fh.read() #legge tutte le righe del file
+print(righe)
+fh.close()
+
+
+#######################################################################
+fh = open("file.txt")                                                 #
+righe = fh.readlines() #mi lege tutto file e mi restituisce una lista #
+print(righe)                                                          #
+fh.close()                                                            #
+#######################################################################
+
+fh = open("file.txt")
+righe = fh.readlines(14) #si blocca alla riga che arriva il 14 carattere
 print(righe)
 fh.close()
