c8c79583eefb1cd9b85018997de0ac0df683d536
MatDev <cod4882@gmail.com>
2024-06-09 17:11:44 +0200

    a

--- Lezioni/Lez8/Esercizi/terzo.py
+++ Lezioni/Lez8/Esercizi/terzo.py
@@ -9,3 +9,24 @@ Provare il programma con testi classici come la Divina Commedia di Dante Alighie
 reperibile sul sito del progetto Gutenberg.
     
 ''' 
+
+class Classe:
+    def __init__(self, nomefile):
+        self.nomefile = nomefile
+
+    def scriviFile(self, contenuto):
+        file = open(self.nomefile, 'w')
+        file.write(contenuto)
+        file.close()
+
+    def leggiFile(self):
+        file = open(self.nomefile, 'r')
+        contenuto = file.read()
+        file.close()
+        return contenuto
+
+    def trovaParoleLunghe(self, n=1):
+        contenuto = self.leggiFile()
+        parole = contenuto.split()
+        paroleOrdinate = sorted(parole, key=len, reverse=True)
+        return paroleOrdinate[:n]
