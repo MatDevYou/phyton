a1716acb3732a661bc00eb94dfcc9f219950727e
MatDev <cod4882@gmail.com>
2024-06-09 10:39:11 +0200

    a

--- Lezioni/Lez8/Esercizi/primo.py
+++ Lezioni/Lez8/Esercizi/primo.py
@@ -1,10 +1,37 @@
 '''
-Autore: Matteo Meringolo
-Data: 04/06/2024
+    Autore: Matteo Meringolo
+    Data: 04/06/2024
 
-Titolo:
+    Titolo:
+    
 '''
 
-stringa = input("Inserisci la stringa a tuo piacere")
+def salva_stringa():
+    # Leggere la stringa da tastiera
+    stringa = input("Inserisci una stringa: ")
+    
+    # Salvare la stringa nel file "stringa.txt"
+    file = open("stringa.txt", "w")
+    file.write(stringa)
+    file.close()
+    
+    print("Stringa salvata su 'stringa.txt'.")
 
-fh = openi
+def verifica_salvataggio():
+    # Leggere il contenuto del file "stringa.txt"
+    try:
+        file = open("stringa.txt", "r")
+        contenuto = file.read()
+        file.close()
+        print("Contenuto del file 'stringa.txt':")
+        print(contenuto)
+    except FileNotFoundError:
+        print("Errore: Il file 'stringa.txt' non esiste.")
+
+# Chiamare direttamente le funzioni
+salva_stringa()
+verifica_salvataggio()
+
+
+
+
