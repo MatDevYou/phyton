77cc1bd3126163c53f43a168eb8046512c4f85f0
MatDev <cod4882@gmail.com>
2024-06-09 19:00:10 +0200

    a

--- Lezioni/Lez8/Esercizi/quarto.py
+++ Lezioni/Lez8/Esercizi/quarto.py
@@ -6,3 +6,27 @@ Titolo:Scrivere un programma che permetta di copiare il contenuto di un file in 
     
 '''
 
+class File:
+    def __init__(self, nomefile):
+        self.nomefile = nomefile
+
+    def leggiFile(self):
+        file = open(self.nomefile, 'r')
+        contenuto = file.read()
+        file.close()
+        return contenuto
+
+    def scriviFile(self, contenuto):
+        file = open(self.nomefile, 'w')
+        file.write(contenuto)
+        file.close()
+
+def copiaFile(nomefileSorgente, nomefileDestinazione):
+    fileSorgente = File(nomefileSorgente)
+    contenuto = fileSorgente.leggiFile()
+    fileDestinazione = File(nomefileDestinazione)
+    fileDestinazione.scriviFile(contenuto)
+    print(f"Contenuto copiato da '{nomefileSorgente}' a '{nomefileDestinazione}'.")
+
+# Esempio di utilizzo
+copiaFile('Lezioni/Lez8/Esercizi/stringa.txt', 'Lezioni/Lez8/Esercizi/output.txt')
